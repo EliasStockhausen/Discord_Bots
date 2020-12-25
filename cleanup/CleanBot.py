296b5b09181d626bb61a810fc4240f36b6de6836
Elias <elias@famst.de>
2020-12-25 13:26:19 +0100

    Initial

---
+++ cleanup/CleanBot.py
@@ -0,0 +1,39 @@
+# bot.py
+import os
+import logging
+
+import discord
+
+from discord.ext import commands
+
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('CleanBot')
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command(name='cleanup')
+async def clean(ctx):
+    channel = ctx.channel
+    messages = await channel.history(limit=500).flatten()
+    for msg in messages:
+        logger.info(f'Message: {msg.created_at} by {msg.author.name} is pinned  {msg.pinned}')
+
+        if not msg.pinned:
+            logger.info(f'Deleting Message')
+            await msg.delete()
+
+bot.run(TOKEN)
